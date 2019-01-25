5d0b67fcd57a89c9f89a5d7baad92d8bca6ffa97
Emil Gustavsen <emilgustavsen@gmail.com>
2019-01-25 12:24:34 +0100

    Initial commit

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './BuildControls.module.css'
+
+const buildControls = (props) => {
+    <div className={styles.BuildControls}>
+        
+    </div>
+};
+export default buildControls;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Layout.module.css'
+
+const layout = (props) => (
+    <>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={styles.content}>
+            {props.children}
+        </main>
+    </>
+)
+
+export default layout;
